0649c8c5d748fe674fbcc493462293ebdcb04802
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-07-20 11:04:44 +0100

    operative

--- gh-org-members.js
+++ gh-org-members.js
@@ -1,10 +1,105 @@
 const ins = require("util").inspect;
-const balanced = require('balanced-match');
 
 const shell = require('shelljs');
 const { Command } = require('commander');
-const { setDefaultOrg, fzfGetOrg } = require('@crguezl/gh-utilities');
-const { off } = require("process");
+const { getMembers, showError, setDefaultOrg, fzfGetOrg } = require('@crguezl/gh-utilities');
+//const homedir = require('os').homedir();
+
+function writeMembers(members, options) {
+  if (options.json) {
+    console.log(JSON.stringify(members, null, 2));
+    process.exit(0);
+  }
+  
+  let set = new Set(Object.keys(members[0]));
+  set.delete('regexp');
+  set.delete('json');
+  set.delete('org');
+  set.delete('debug');
+  
+  members.forEach(x => {
+    let output = [];
+    for (opt of set) {
+      //console.log(opt);
+      output.push(`"${x[opt]}"`);
+    }
+    console.log(output.join(","));
+  });
+  process.exit(0)
+}
+
+function main(members, options, org) {
+
+  members = members.filter(m => regexp.test(m.name) || regexp.test(m.login))
+  // read the csv file and decorate the filtered members with the additional info
+  // Get the teams of those members and decorate the result with the team info
+  
+  // Decorate: add site
+  members.forEach(x => x.site = `https://${x.login}.github.io`)
+  
+  members.map(x => x.orgurl = `https://github.com/orgs/${org}/people/${x.login}`)
+  
+  members.forEach(x => x.fullname = `${x?.name || x?.login}`);
+  
+  let csvFilePath;
+
+  if (options.csv) {
+    if (options.pathcsv) {
+      csvFilePath = options.pathcsv;
+    } else { // Get the last file matching /${org}.*\.csv/ in the downloads folder
+      const downloadsFolder = require('downloads-folder');
+      const path = require('path');
+      const fs = require('fs');
+  
+      var downloadsDir = downloadsFolder(); // your downloads directory
+      let escapedOrg = org.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+      let regexp = new RegExp(`^${escapedOrg}.*\\.csv$`);
+      var files = fs.readdirSync(downloadsFolder()).filter(file => file.match(regexp)).map(file => path.join(downloadsDir, file));
+      //console.log(files);
+      if (files.length === 0) {
+        console.error(`No csv file matching regexp pattern /${regexp.source}/ found for organization "${org}" in folder "${downloadsDir}"`);
+        process.exit(1);
+      }
+  
+      // sort by date
+      files.sort((a, b) => fs.statSync(a).mtime.getTime()-fs.statSync(b).mtime.getTime());
+      //console.log(files);
+          
+      csvFilePath = files.pop(); // get the last file matching the org name in the downloads folder  
+    }
+    //console.log(csvFilePath);
+
+    const csv=require('csvtojson');
+    csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        jsonObj.forEach((x, j) => {
+          members.forEach(m => {
+            if (m.login === x.login) {
+              //console.log(`${m.login},${x.id}`);
+              if (x.id) m.id = x.id;
+              //console.log(options.csv)
+              if (Array.isArray(options.csv)) {
+                options.csv.forEach(c => {
+                  m[c] = x[c];
+                }
+                )
+              } else {
+                m = Object.assign(m, x);
+              }
+            }
+          });
+        });
+        writeMembers(members, options);
+    })
+    .catch(err =>{
+      console.error(`Error trying to  read file "${csvFilePath}"\n${err}`);
+      process.exit(1);
+    })  
+  } else {
+    writeMembers(members, options);
+  };
+} // end main
 
 const program = new Command();
 program.version(require('./package.json').version);
@@ -20,6 +115,7 @@ program
   .option('-w, --orgurl', 'show github user url as a member of the org')
   .option('-s, --site', 'show url of the members github pages web sites')
   .option('-c, --csv [field...]', 'shows the values of the fields of the organization csv')
+  .option('-p, --pathcsv <csv file>', 'path to the csv file')
   .option('-o --org <org>', 'default organization')
   .option('   --default', 'Set selected "org" as default organization for future uses');
 
@@ -33,109 +129,12 @@ program.addHelpText('after', `
 
 
 program.parse(process.argv);
-
-
 const options = program.opts();
 
 const deb = (...args) => {
-  console.log(ins(...args, { depth: null }));
+  console.error(ins(...args, { depth: null }));
 };
-
-//deb(options);
-
-function getMembers(org) {
-  // Get the list of members of the organization
-  let result = ghCont(`api graphql --paginate -f query='
-  query($endCursor: String) {
-    organization(login: "${org}") {
-      membersWithRole(first: 10, after:$endCursor) {
-        pageInfo {
-          hasNextPage,
-          endCursor
-        }
-        edges {
-          node {
-            login
-            name
-            url
-          }
-          role
-        }
-      }
-    }
-  }
-  '
-  `);
-
-  /* example of output
-  {
-  "data": {
-    "organization": {
-      "membersWithRole": {
-        "pageInfo": {
-          "hasNextPage": true,
-          "endCursor": "Y3Vyc29yOnYyOpHOAVenaQ=="
-        },
-        "edges": [
-          {
-            "node": {
-              "login": "crguezl",
-              "name": "Casiano Rodriguez-Leon"
-            },
-            "role": "ADMIN"
-          },
-          {
-            "node": {
-              "login": "casiano",
-              "name": "Casiano"
-            },
-            "role": "MEMBER"
-          },
-          {
-            "node": {
-              "login": "amarrerod",
-              "name": "Alejandro Marrero"
-            },
-            "role": "MEMBER"
-          },
-          {
-            "node": {
-              "login": "ivan-ga",
-              "name": "Iván González"
-            },
-            "role": "MEMBER"
-          }
-        ]
-      }
-    }
-  }
-  }
-  */
-
-  if (result.stderr) {
-    let messages = JSON.parse(result.stdout).errors.map(x => x.message);
-    console.log(messages.join("\n"));
-    process.exit(1);
-  }
-
-  rout = result.stdout;
-  //console.log(rout);
-  let chunkInfo, members = [];
-
-  while (chunkInfo = balanced('{', '}', rout)) {
-    //console.log(chunkInfo);
-    let currentObj = JSON.parse('{' + chunkInfo.body + '}');
-    let currMembers = currentObj.data.organization.membersWithRole.edges.map(x => {
-      let y = x.node;
-      y.role = x.role.toLowerCase();
-      return y;
-    });
-    //console.log(currMembers); 
-    members = members.concat(currMembers);
-    rout = chunkInfo.post;
-  }
-  return members;
-} // end getMembers
+// deb(options);
 
 if (!shell.which('git')) {
   showError('Sorry, this extension requires git installed!');
@@ -143,22 +142,6 @@ if (!shell.which('git')) {
 if (!shell.which('gh')) {
   showError('Sorry, this extension requires GitHub Cli (gh) installed!');
 }
-
-function showError(error) {
-  if (error) {
-    console.error(`Error!: ${error}`);
-    process.exit(1);
-  }
-}
-
-function shContinue(executable, arg, cb) {
-  let command = `${executable} ${arg}`;
-  //deb(command);
-  let result = shell.exec(command, { silent: true }, cb);
-  return result;
-}
-
-const ghCont = (arg, cb) => shContinue("gh", arg, cb);
 
 debugger;
 
@@ -182,34 +165,6 @@ if (!org) { // set org to the owner of the current repo
 
 let members = getMembers(org);
 
-members = members.filter(m => regexp.test(m.name) || regexp.test(m.login))
-// read the csv file and decorate the filtered members with the additional info
-// Get the teams of those members and decorate the result with the team info
+main(members, options, org);
 
-// Decorate: add site
-members.forEach(x => x.site = `https://${x.login}.github.io`)
 
-members.map(x => x.orgurl = `https://github.com/orgs/${org}/people/${x.login}`)
-
-members.forEach(x => x.fullname = `${x?.name || x?.login}`);
-
-if (options.json) {
-  console.log(JSON.stringify(members, null, 2));
-  process.exit(0);
-}
-
-let set = new Set(Object.keys(options));
-set.delete('regexp');
-set.delete('json');
-set.delete('org');
-set.delete('debug');
-
-members.forEach(x => {
-  let output = [];
-  for (opt of set) {
-    //console.log(opt);
-    output.push(`"${x[opt]}"`);
-  }
-  console.log(output.join(","));
-});
-process.exit(0)
